144beb4172e1df15148410aec0ca554f410cc44b
Владимир Стальмаков <akummail@gmail.com>
2018-04-19 01:29:33 +0300

    тесты с protobuf

--- test_protobuf/main.cpp
+++ test_protobuf/main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 
-#include "pb.h"
-#include "pb_common.h"
-#include "pb_decode.h"
-#include "pb_encode.h"
-#include "TemperatureProtocol.pb.h"
+#include "pb/pb.h"
+#include "pb/pb_common.h"
+#include "pb/pb_decode.h"
+#include "pb/pb_encode.h"
+#include "pb/BaseMessage.pb.h"
+#include "pb/Temperature.pb.h"
+
+void printBytes(const uint8_t *bufferAuth, size_t sizeAuth) {
+    std::cout << "message_length " << sizeAuth << std::endl;
+    std::cout << "message " << std::endl;
+    for (uint32_t i = 0; i < sizeAuth; i++) {
+        std::cout << std::hex << (unsigned int) (bufferAuth[i]) << " ";
+    }
+    std::cout << std::endl;
+}
 
 
 bool encode_string_key(pb_ostream_t *stream, const pb_field_t *field, void *const *arg) {
-    printf("encode_string...\n");
+
 
     const char *str = "53ae8ff6-3d8c-11e8-b467-0ed5f89f718b";
 
     if (!pb_encode_tag_for_field(stream, field)) {
         return false;
     }
-
+    printf("encode_string...\n");
     return pb_encode_string(stream, (uint8_t *) str, strlen(str));
 }
 
@@ -38,43 +48,104 @@ bool encode_baseMessage(pb_ostream_t *stream, const pb_field_t messagetype[], co
 }
 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+bool encode_temperatureMessage(pb_ostream_t *stream, const pb_field_t messagetype[], const void *message) {
+    const pb_field_t *field;
+    for (field = Temperature_fields; field->tag != 0; field++) {
+        if (field->ptr == messagetype) {
+            /* This is our field, encode the message using it. */
+            if (!pb_encode_tag_for_field(stream, field)) {
+                return false;
+            }
 
-    uint8_t buffer[128];
-    size_t message_length;
+            return pb_encode_submessage(stream, messagetype, message);
+        }
+    }
+
+    /* Didn't find the field for messagetype */
+    return false;
+}
+
+
+bool encode_baseMessage1(pb_ostream_t *stream, const pb_field_t messagetype[], const void *message) {
+    const pb_field_t *field;
+    for (field = BaseMessage_fields; field->tag != 0; field++) {
+        if (field->ptr == Temperature_fields) {
+
+            if (!pb_encode_tag_for_field(stream, field)) {
+                return false;
+            }
+            // return
+
+
+
+            //  This is our field, encode the message using it.
+
+
+            return encode_temperatureMessage(stream, messagetype, message);
+        }
+    }
+
+    /* Didn't find the field for messagetype */
+    return false;
+}
+
+
+size_t auth(uint8_t *buffer, size_t bufsize) {
     bool status;
 
 
-    AuthRequest authRequest = AuthRequest_init_default;
+    Auth authRequest = Auth_init_default;
 
     authRequest.type = 1;
     authRequest.version = 1;
     authRequest.apiKey.funcs.encode = encode_string_key;
 
-    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, bufsize);
 
-    status = encode_baseMessage(&stream, AuthRequest_fields, &authRequest);
+    status = encode_baseMessage(&stream, Auth_fields, &authRequest);
 
-    // status = pb_encode(&stream, AuthRequest_fields, &authRequest);
-    message_length = stream.bytes_written;
-
-
-    std::cout << "message_length " << message_length << std::endl;
-    std::cout << "message " << std::endl;
-    for (uint32_t i = 0; i < message_length; i++) {
-        std::cout << std::hex << (unsigned int) (buffer[i]) << " ";
-    }
-    std::cout << std::endl;
-
-
-
-    /* Then just check for any errors.. */
     if (!status) {
         printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
-        return 1;
+
     }
 
+    return stream.bytes_written;
+
+}
+
+
+size_t notify(uint8_t *buffer, size_t bufsize) {
+
+    Notify notify = Notify_init_default;
+    notify.current = 1;
+
+
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, bufsize);
+    bool status = encode_baseMessage1(&stream, Notify_fields, &notify);
+    if (!status) {
+        printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
+
+    }
+    return stream.bytes_written;
+}
+
+
+int main() {
+    std::cout << "Start" << std::endl;
+
+/*    {
+        uint8_t bufferAuth[150];
+        size_t sizeAuth = auth(bufferAuth, sizeof(bufferAuth));
+        printBytes(bufferAuth, sizeAuth);
+    }*/
+
+    {
+        uint8_t bufferNotify[128];
+        size_t sizeNotify = notify(bufferNotify, sizeof(bufferNotify));
+        printBytes(bufferNotify, sizeNotify);
+
+    }
 
     return 0;
 }
+
